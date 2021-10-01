6d1f078ebe25ac1fe2b1d1d05b052809757693bc
codewithwaleed <codewithwaleed@gmail.com>
2021-10-01 06:56:26 +0500

    task done

---
+++ my-function.js
@@ -0,0 +1,7 @@
+function myFunction (params) {
+  const {query} = params;
+  const {keyword = 'no value available for keyword'} = query
+  return `Kema says ${keyword}`
+}
+
+module.exports = { myFunction };
